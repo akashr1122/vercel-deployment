6b178b1d28970c4273012f6f6371d1b2797d533b
akashr1122 <akashsingh4321a@gmail.com>
2023-12-30 01:12:20 +0530

    add2

--- index.js
+++ index.js
@@ -1,5 +1,41 @@
-const app = require('./src/app.js');
+// const app = require('./src/app.js');
 const port = process.env.PORT || 8001;
+
+const express       = require('express');
+var bodyParser      = require('body-parser');
+var cors            = require('cors');
+var dotenv          = require('dotenv');
+var MongoClient     = require('mongoose');
+
+const app = express();
+app.use(cors());
+
+// Enable environment variable
+dotenv.config();
+
+// Connection to MongoDB server
+MongoClient.connect(process.env.MONGO_URI,{useNewUrlParser:true, useUnifiedTopology: true},function(){
+    console.log('Connect to MongoDB');
+});
+
+MongoClient.set('useFindAndModify', false);
+MongoClient.set('useCreateIndex', true);
+
+app.use(bodyParser.urlencoded({
+    extended:true,
+    limit: '10mb',
+    parameterLimit: 100000
+}));
+app.use(bodyParser.json()); // take json data
+
+app.use(express.static('public'));
+app.use('/',express.static('public'));
+
+process.env.TZ = 'Asia/Kolkata'; // here is the magical line
+
+require('./src/router/admin')(app);
+require('./src/router/app')(app);
+// module.exports = app;
 
 // Server
 app.listen(port, () => {
